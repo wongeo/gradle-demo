552c212900ed8522549cd752dffed3a33af5d44c
WangJing <wj148202@alibaba-inc.com>
2017-11-01 19:06:33 +0800

    first

---
+++ gradletest/src/main/java/com/feng/plug/PluginImpl.groovy
@@ -0,0 +1,17 @@
+package com.feng.plug
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class PluginImpl implements Plugin<Project> {
+
+    void apply(Project project) {
+        project.task('testTask') << {
+            println "Hello gradle plugin"
+        }
+    }
+
+    def abc() {
+        return ""
+    }
+}
